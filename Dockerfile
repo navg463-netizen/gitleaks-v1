7df52e5e3ec3cde881a2d77481dc742ba926fdd5
navg463@gmail.com <navg463@gmail.com>
2025-10-30 05:43:21 +0000

    gitleaksupdated

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ WORKDIR /app
 COPY python_app/ .
 RUN pip install flask
 ENV PORT=5000
-CMD ["python", "app.py"]
 EXPOSE 5000
+CMD ["python", "app.py"]
--- python_app/app.py
+++ python_app/app.py
@@ -1,17 +1,4 @@
-# # app.py
-# import os
 
-# def connect_to_service():
-#     # ❌ Hardcoded API key — will be caught by GitLeaks (generic-api-key / AWS / Token patterns)
-#     API_KEY = "ghp_abCDeFgHijKLMNOPqrstuVWxyZ012345678"
-    
-#     print("Connecting to external services with the provided API key...")
-#     print(f"Using API_KEY: {API_KEY[:6]}********")
-
-#     return True
-
-# if __name__ == "__main__":
-#     connect_to_service()
 
 
 import os
